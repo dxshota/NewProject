be3d52e81372d137b00112e50bcada0d494cb0e7
dxshota <dxsyota1230@icloud.com>
2024-10-16 00:33:17 +0900

    選手座標をオーダー２の浮動小数に変更

---
+++ database/migrations/2024_10_15_152305_update_player_positions_to_decimal_in_players_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('players', function (Blueprint $table) {
+            // player_position_x と player_position_y のカラムを decimal 型に変更 (小数点2桁)
+            $table->decimal('player_position_x', 8, 2)->change();
+            $table->decimal('player_position_y', 8, 2)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('players', function (Blueprint $table) {
+            //
+        });
+    }
+};
