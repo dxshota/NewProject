1ff9a6f02c5d9b5b13d02adcc17d29d13eaae270
dxshota <dxsyota1230@icloud.com>
2024-11-16 11:15:13 +0900

    final

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use database\Seeders\PositionPlayerTemplateSeeder as PPTSeeder;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call(
             TemplateSeeder::class,
-            PositionPlayerTemplateSeeder::class
+            PPTSeeder::class
     );
     }
 }
